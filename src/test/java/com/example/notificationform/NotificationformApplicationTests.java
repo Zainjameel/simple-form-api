78dab6c4b15784a937d1aac272985c36e5646ffe
zain jameel <jameel.zain@yahoo.com>
2021-10-09 14:56:59 -0500

    create spring project

---
+++ src/test/java/com/example/notificationform/NotificationformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
